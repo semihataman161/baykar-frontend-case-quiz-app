ee70ec36aa17b43df35d323f7f9b02cae6b48468
semih <semih@uraltelekom.com>
2024-06-27 20:26:04 +0300

    Added more informations to table.

--- src/components/ResultsTable/index.tsx
+++ src/components/ResultsTable/index.tsx
@@ -1,31 +1,36 @@
 import React from "react";
+import { ITableData } from "../../types/Question";
 
 interface IResultsTableProps {
-    answers: {
-        question: string;
-        options: string[];
-        answer: string | null;
-    }[];
+    tableData: ITableData[];
 }
 
-const ResultsTable: React.FC<IResultsTableProps> = ({ answers }) => {
+const ResultsTable: React.FC<IResultsTableProps> = ({ tableData }) => {
     return (
-        <div className="text-center bg-white p-6 rounded-lg shadow-md w-full max-w-md mx-auto">
-            <h2 className="mb-4">Sonuçlar</h2>
+        <div className="text-center bg-white p-6 rounded-lg shadow-md w-full mx-auto">
+            <h2 className="mb-4 font-bold">Sonuçlar</h2>
             <table className="w-full table-auto">
                 <thead>
                     <tr>
                         <th className="px-4 py-2">Soru</th>
                         <th className="px-4 py-2">Şıklar</th>
                         <th className="px-4 py-2">Cevabınız</th>
+                        <th className="px-4 py-2">Doğru Cevap</th>
+                        <th className="px-4 py-2">Doğru / Yanlış</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {answers.map((answer, index) => (
+                    {tableData.map((element, index) => (
                         <tr key={index}>
-                            <td className="border px-4 py-2">{answer.question}</td>
-                            <td className="border px-4 py-2">{answer.options.join(', ')}</td>
-                            <td className="border px-4 py-2">{answer.answer}</td>
+                            <td className="border px-4 py-2">{element.question}</td>
+                            <td className="border px-4 py-2">{element.options.join(', ')}</td>
+                            <td className="border px-4 py-2">
+                                {element.selectedAnswer ?? "-"}
+                            </td>
+                            <td className="border px-4 py-2">{element.trueAnswer}</td>
+                            <td className={`border px-4 py-2 ${element.selectedAnswer === element.trueAnswer ? 'bg-green-100 text-green-600 font-bold' : 'bg-red-100 text-red-600'}`}>
+                                {element.selectedAnswer ?? "-"}
+                            </td>
                         </tr>
                     ))}
                 </tbody>
